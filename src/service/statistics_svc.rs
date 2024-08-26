c34292b11b850d7cf0ee080e40e51898e2f33dd1
NPCDW <chenyongli0520@gmail.com>
2024-08-26 16:53:22 +0800

    .

--- src/service/statistics_svc.rs
+++ src/service/statistics_svc.rs
@@ -144,7 +144,7 @@ pub async fn collect_day_data(app_state: &AppState, statistic_date: NaiveDate) -
                     CycleStatisticMethod::OnlyOut => cycle_day_uplink_traffic_usage,
                     CycleStatisticMethod::SumInOut => cycle_day_uplink_traffic_usage + cycle_day_downlink_traffic_usage,
                 };
-                text = format!("{} 计入流量: {}\n{} ~ {} 周期用量: {}/{}\n上一周期已结束", text, traffic_show(yesterday_traffic_usage), pre_start, pre_end, traffic_show(cycle_traffic_usage), traffic_show(cycle.traffic_limit));
+                text = format!("{} 计入流量: {}\n{} ~ {} 上传: {} 下载: {} 计入流量: {}/{}\n上一周期已结束", text, traffic_show(yesterday_traffic_usage), pre_start, pre_end, traffic_show(cycle_day_uplink_traffic_usage), traffic_show(cycle_day_downlink_traffic_usage), traffic_show(cycle_traffic_usage), traffic_show(cycle.traffic_limit));
             } else {
                 let (cycle_day_uplink_traffic_usage, cycle_day_downlink_traffic_usage) = monitor_day_mapper::get_daterange_data(cycle.current_cycle_start_date, cycle.current_cycle_end_date, &app_state.db_pool).await?.unwrap_or((0, 0));
                 let cycle_traffic_usage = match cycle.statistic_method {
@@ -152,7 +152,7 @@ pub async fn collect_day_data(app_state: &AppState, statistic_date: NaiveDate) -
                     CycleStatisticMethod::OnlyOut => cycle_day_uplink_traffic_usage,
                     CycleStatisticMethod::SumInOut => cycle_day_uplink_traffic_usage + cycle_day_downlink_traffic_usage,
                 };
-                text = format!("{} 计入流量: {}\n{} ~ {} 周期用量: {}/{}\n距下次重置: {}天", text, traffic_show(yesterday_traffic_usage), cycle.current_cycle_start_date, cycle.current_cycle_end_date, traffic_show(cycle_traffic_usage), traffic_show(cycle.traffic_limit), (cycle.current_cycle_end_date - chrono::Local::now().date_naive()).num_days() + 1);
+                text = format!("{} 计入流量: {}\n{} ~ {} 上传: {} 下载: {} 计入流量: {}/{}\n距下次重置: {}天", text, traffic_show(yesterday_traffic_usage), cycle.current_cycle_start_date, cycle.current_cycle_end_date, traffic_show(cycle_day_uplink_traffic_usage), traffic_show(cycle_day_downlink_traffic_usage), traffic_show(cycle_traffic_usage), traffic_show(cycle.traffic_limit), (cycle.current_cycle_end_date - chrono::Local::now().date_naive()).num_days() + 1);
             }
         }
         let url = format!("https://api.telegram.org/bot{}/sendMessage", tg.bot_token);
